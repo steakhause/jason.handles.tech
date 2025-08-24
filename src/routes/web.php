6f37084c66d41897ebcb4f62b26be1e93b39beb2
Jason Ryszka <jason.ryszka@gmail.com>
2025-08-24 22:43:40 +0000

    CI/CD testing. No changes

--- src/routes/web.php
+++ src/routes/web.php
@@ -21,6 +21,7 @@ Route::get('/resume', function () {
     return view('resume');
 })->name('resume');
 
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
